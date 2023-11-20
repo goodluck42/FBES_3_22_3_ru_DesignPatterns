fb0e9da5edfb507fbf61f9c97a278c28b7550755
xrlogic1 <xrlogic1@gmail.com>
2023-11-20 20:55:47 +0400

    *

---
+++ Facade/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine();
+
+
+
+public class AuthService
+{
+}
+
+public class RegisterService
+{
+}
+
+public class Facade
+{
+    public Facade(AuthService authService, RegisterService registerService)
+    {
+        AuthService = authService;
+        RegisterService = registerService;
+    }
+
+    public AuthService AuthService { get; set; }
+    public RegisterService RegisterService { get; set; }
+
+    // ...
+}
+
+public static class StaticFacade
+{
+    public static AuthService? AuthService { get; set; }
+    public static RegisterService? RegisterService { get; set; }
+
+    // ...
+}
